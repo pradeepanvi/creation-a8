8be9c8fb3241eb969824b96527ef68fd1eee30d5
pradeepanvi <pradeepkumar.anvi@gmail.com>
2019-07-20 22:49:46 +0530

    header and footer done

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'creation-a8';
+
+  ngOnInit() {
+    var header = document.querySelector('header');
+    //Sticky header
+    window.onscroll = function () {
+      window.scrollY > 300 ? header.classList.add('fixed') : header.classList.remove('fixed');
+    }
+  }
+}
+
